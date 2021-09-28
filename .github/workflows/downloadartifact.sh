32adda2b0c0743d49f0785680982e1735bf8fddb
PAiCBD <development-team@paicbd.com>
2021-09-28 14:12:22 -0400

    Update the github actions

---
+++ .github/workflows/downloadartifact.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# see README.md for more details
+wget https://www.dialogic.com/files/DSI/developmentpackages/linux/dpklnx.Z
+tar --no-same-owner -zxvf dpklnx.Z
+mvn install:install-file -DgroupId=com.vendor.dialogic -DartifactId=gctapi -Dversion=6.7.1 -Dpackaging=jar -Dfile=./JAVA/gctApi.jar
+
+wget http://www.datanucleus.org/downloads/maven2/com/sun/jdmk/jmxtools/1.2.1/jmxtools-1.2.1.jar
+mvn install:install-file -DgroupId=com.sun.jdmk -DartifactId=jmxtools -Dversion=1.2.1 -Dpackaging=jar -Dfile=jmxtools-1.2.1.jar
+
